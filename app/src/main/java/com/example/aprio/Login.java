1bc1b8396ecfdb9ecaaf23b8daea795247ad1b03
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-12 23:16:11 -0600

    merge branch sign_user and Login

--- app/src/main/java/com/example/aprio/Login.java
+++ app/src/main/java/com/example/aprio/Login.java
@@ -1,28 +1,101 @@
 package com.example.aprio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Login extends AppCompatActivity {
+
+    @BindView(R.id.etEmailLogin)
+    EditText email;
+    @BindView(R.id.etPasswordLogin)
+    EditText password;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.tvSignupAsSeller)
+    TextView tvSaS;
+    @BindView(R.id.tvSignup)
+    TextView tvSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
-        TextView SaS=findViewById(R.id.tvSignupAsSeller);
-        SaS.setOnClickListener(new View.OnClickListener() {
+        //Session
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            //todo: Check the category of user
+            GoToSpecificPage(currentUser);
+            //todo: intent map activity
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Login.this,Signup_as_vendor.class));
+                String mEmail = email.getText().toString();
+                String mPassword = password.getText().toString();
+
+                Log.d("APP_MESS", "You clicked ! " + mEmail + " " + mPassword);
+
+                ParseUser.logInInBackground(mEmail, mPassword, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(Login.this, "Erreur : " + e.getMessage(), Toast.LENGTH_LONG).show();
+                            e.printStackTrace();
+                            return;
+                        }
+                        //todo: Check the category of user
+                        GoToSpecificPage(user);
+                        //todo: intent map distinct activity
+                    }
+                });
+            }
+        });
+
+        tvSaS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Signup_as_vendor.class));
+            }
+        });
+
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,SignUser.class));
             }
         });
     }
+
+    private void GoToSpecificPage(ParseUser user) {
+        if (user.getBoolean("Category")) {
+            //Vendor
+            Log.d("APP_MESS", "I'm a vendor");
+            //Toast.makeText(Login.this,"I'm a vendor",Toast.LENGTH_LONG).show();
+        } else {
+            //User
+            Log.d("APP_MESS", "I'm a user");
+            //Toast.makeText(Login.this,"I'm a user",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/aprio/SignUser.java
+++ app/src/main/java/com/example/aprio/SignUser.java
@@ -1,14 +1,87 @@
 package com.example.aprio;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SignUser extends AppCompatActivity {
+
+    @BindView(R.id.edt_name)
+    EditText edtName;
+    @BindView(R.id.edt_email)
+    EditText edtEmail;
+    @BindView(R.id.edt_password)
+    EditText edtPassword;
+    @BindView(R.id.edt_cfm_password)
+    EditText edtCfmPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.tvSignup)
+    TextView tvSignup;
+
+    static final  String TAG = "SignUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_user);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnLogin, R.id.tvSignup})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                String username = edtName.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+                String password_confirm = edtCfmPassword.getText().toString().trim();
+                String email = edtEmail.getText().toString().trim();
+
+                if(password.equals(password_confirm))
+                    Sign_user(username, password, email);
+                else
+                    Toast.makeText(getApplicationContext(),"No Match Password", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvSignup:
+                break;
+        }
+    }
+
+    public void Sign_user(String username, String password,String email)
+    {
+        ParseUser user = new ParseUser();
+
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setEmail(email);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null){
+                    Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    ParseUser.logOut();
+                    Log.e(TAG,e.getMessage());
+                }
+            }
+        });
     }
 }
