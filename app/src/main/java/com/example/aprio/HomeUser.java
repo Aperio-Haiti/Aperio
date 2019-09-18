416f453ab3c789f86bc88a5ae38563df373c8f42
sauvelino <sauvelino@outlook.com>
2019-09-18 06:29:55 -0400

    version with little modification (drawer in homeuser 40%)

--- app/src/main/java/com/example/aprio/HomeUser.java
+++ app/src/main/java/com/example/aprio/HomeUser.java
@@ -4,17 +4,36 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeUser extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 @Override
 protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    drawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+    actionBarDrawerToggle=new ActionBarDrawerToggle(HomeUser.this,drawerLayout,R.string.Open,R.string.Close);
+    drawerLayout.addDrawerListener(actionBarDrawerToggle);
+    actionBarDrawerToggle.syncState();
+   // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
 
 @Override
 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem){
