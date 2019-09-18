f033f557337cc33fc2f4ef9ff8d10f719672060f
sauvelino <sauvelino@outlook.com>
2019-09-18 06:47:52 -0400

    version with little modification (drawer 40%)

--- app/src/main/java/com/example/aprio/HomeSeller.java
+++ app/src/main/java/com/example/aprio/HomeSeller.java
@@ -1,7 +1,9 @@
 package com.example.aprio;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
@@ -12,7 +14,8 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeSeller extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,12 @@ public class HomeSeller extends AppCompatActivity implements BottomNavigationVie
         BottomNavigationView tabs = findViewById(R.id.tabs);
         tabs.setOnNavigationItemSelectedListener(this);
         tabs.setSelectedItemId(R.id.btnMapFragment);
+
+        drawerLayout= findViewById(R.id.drawer1);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(HomeSeller.this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private Boolean LoadFragment(Fragment fragment) {
@@ -56,5 +65,15 @@ public class HomeSeller extends AppCompatActivity implements BottomNavigationVie
 
     public void add(View view) {
         startActivity(new Intent(this,addproduct.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
