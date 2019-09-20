37d3b353026c528898af5266d82492ff0f2f7422
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-19 18:33:17 -0600

    Map_vendor

--- app/src/main/java/com/example/aprio/FragmentMapSeller.java
+++ app/src/main/java/com/example/aprio/FragmentMapSeller.java
@@ -9,10 +9,43 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentMapSeller  extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FragmentMapSeller  extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.mapfragment, container, false);
+        View view = inflater.inflate(R.layout.mapfragment, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng port_au_prince = new LatLng(18.547863, -72.325246);
+        mMap.addMarker(new MarkerOptions().position(port_au_prince).title("Marker in port-au-prince"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(port_au_prince));
+
     }
 }
