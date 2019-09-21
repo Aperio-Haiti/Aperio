f784d0324fb1803cf3c71031c35a3a5d2ba0effe
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-21 14:09:54 -0600

    Map_Vendor

--- app/src/main/java/com/example/aprio/FragmentMapSeller.java
+++ app/src/main/java/com/example/aprio/FragmentMapSeller.java
@@ -3,6 +3,7 @@ package com.example.aprio;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,9 +22,11 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -42,13 +45,14 @@ public class FragmentMapSeller extends Fragment implements OnMapReadyCallback {
 
     GoogleMap map;
 
-    public static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    public static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    public static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
     private  boolean mLocationPermissionGranted = false;
 
-    FusedLocationProviderClient fusedLocationProviderClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private final static float DEFAULT_ZOOM = 8;
     
 
     @Nullable
@@ -66,6 +70,13 @@ public class FragmentMapSeller extends Fragment implements OnMapReadyCallback {
         Toast.makeText(getContext(),"Map is Ready", Toast.LENGTH_SHORT).show();
         Log.d(TAG, "onMapReady: Map is Ready");
         map = googleMap;
+
+        if(mLocationPermissionGranted){
+            getDeviceLocation();
+            map.setMyLocationEnabled(true);
+            map.getUiSettings().setMyLocationButtonEnabled(false);
+            map.getUiSettings().setZoomControlsEnabled(true);
+        }
     }
 
     private void getLocationPermission(){
@@ -127,6 +138,9 @@ public class FragmentMapSeller extends Fragment implements OnMapReadyCallback {
                     public void onComplete(@NonNull Task task) {
                         if(task.isSuccessful()){
                             Log.d(TAG, "onComplete: found location");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCameraToMyLocationDevice(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()), DEFAULT_ZOOM);
                         }
                         else {
                             Log.d(TAG, "onComplete: current location is null");
@@ -139,5 +153,10 @@ public class FragmentMapSeller extends Fragment implements OnMapReadyCallback {
         }catch (SecurityException e){
             Log.d(TAG, "getDeviceLocation: SecurityException : "+e.getMessage());
         }
+    }
+
+    private void moveCameraToMyLocationDevice(LatLng latLng, float zoom){
+        Log.d(TAG, "moveCameraToMyLocationDevice: moving the camera to: Lat: "+latLng.latitude+" Long: "+latLng.longitude);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
     }
 }
