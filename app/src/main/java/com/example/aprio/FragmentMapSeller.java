b6f793d06fb6e01df784fe074bb0af5a1e8fb088
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-20 23:06:08 -0600

    Map_vendor

--- app/src/main/java/com/example/aprio/FragmentMapSeller.java
+++ app/src/main/java/com/example/aprio/FragmentMapSeller.java
@@ -1,11 +1,20 @@
 package com.example.aprio;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,19 +32,95 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
-public class FragmentMapSeller  extends Fragment implements OnMapReadyCallback {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class FragmentMapSeller extends Fragment implements OnMapReadyCallback {
+
+    @BindView(R.id.ic_magnify)
+    ImageView icMagnify;
+    @BindView(R.id.input_search)
+    EditText inputSearch;
+    @BindView(R.id.ic_gps)
+    ImageView icGps;
     private GoogleMap mMap;
 
     private static final String TAG = "FragmentMapSeller";
     private FusedLocationProviderClient fusedLocationProviderClient;
     public static final float DEFAULT_ZOOM = 15f;
 
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.mapfragment, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    private void init() {
+        Log.d(TAG, "init: initializing");
+        inputSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || keyEvent.getAction() == KeyEvent.ACTION_DOWN
+                        || keyEvent.getAction() == KeyEvent.KEYCODE_ENTER) {
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+        icGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: click gps icon");
+                getLocationDevice();
+            }
+        });
+        hideSoftKeyboard();
+    }
+
+    private void geoLocate() {
+        Log.d(TAG, "geoLocate: geoLocating");
+
+        String searchString = inputSearch.getText().toString();
+
+        Geocoder geocoder = new Geocoder(getContext());
+        List<Address> list = new ArrayList<>();
+
+        try {
+            list = geocoder.getFromLocationName(searchString, 1);
+        } catch (IOException e) {
+            Log.d(TAG, "geoLocate: IOException " + e.getMessage());
+        }
+
+        if (list.size() > 0) {
+            Address address = list.get(0);
+
+            Log.d(TAG, "geoLocate: found a location " + address.toString());
+
+            moveCamera(new LatLng(address.getLatitude(), address.getLongitude()), DEFAULT_ZOOM, address.getAddressLine(0));
+        }
+    }
+
+    private void hideSoftKeyboard() {
+        Window window = getActivity().getWindow();
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     @Override
@@ -47,8 +132,8 @@ public class FragmentMapSeller  extends Fragment implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
     }
 
-    private void getLocationDevice(){
-        Log.d(TAG,"getDeviceLocation: getting the devices current location");
+    private void getLocationDevice() {
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         try {
@@ -56,30 +141,34 @@ public class FragmentMapSeller  extends Fragment implements OnMapReadyCallback {
             location.addOnCompleteListener(new OnCompleteListener() {
                 @Override
                 public void onComplete(@NonNull Task task) {
-                    if(task.isSuccessful())
-                    {
-                        Log.d(TAG,"onComplete : found location!");
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete : found location!");
                         Location currentLocation = (Location) task.getResult();
 
-                        moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),DEFAULT_ZOOM);
+                        moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM, "my Location");
 
-                    }
-                    else
-                    {
-                        Log.d(TAG,"onComplete : current location is null!");
-                        Toast.makeText(getContext(),"unable to get current location",Toast.LENGTH_SHORT).show();
+                    } else {
+                        Log.d(TAG, "onComplete : current location is null!");
+                        Toast.makeText(getContext(), "unable to get current location", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
-        }
-        catch (SecurityException e){
-            Log.e(TAG,"getDeviceLocation: SecurityError "+e.getMessage());
+        } catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityError " + e.getMessage());
         }
     }
 
-    private void moveCamera(LatLng latLng,float zoom){
-        Log.d(TAG,"moveCamera : move the camera to : lat: "+latLng.latitude+",lng: "+latLng.longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,zoom));
+    private void moveCamera(LatLng latLng, float zoom, String title) {
+        Log.d(TAG, "moveCamera : move the camera to : lat: " + latLng.latitude + ",lng: " + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
+        if (!title.equals("my Location")) {
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(title);
+
+            mMap.addMarker(markerOptions);
+        }
+
+        hideSoftKeyboard();
     }
 
     @Override
@@ -87,13 +176,26 @@ public class FragmentMapSeller  extends Fragment implements OnMapReadyCallback {
         mMap = googleMap;
         getLocationDevice();
         mMap.setMyLocationEnabled(true);
+
+        init();
 //        mMap.getUiSettings().setMyLocationButtonEnabled(false);
 //        mMap.getUiSettings().isZoomControlsEnabled();
 
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng port_au_prince = new LatLng(18.547863, -72.325246);
-        mMap.addMarker(new MarkerOptions().position(port_au_prince).title("Marker in port-au-prince"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(port_au_prince));
+        LatLng Depot_Christ_Roi = new LatLng(18.546174, -72.322111);
+        mMap.addMarker(new MarkerOptions().position(Depot_Christ_Roi).title("Christ-Roi Dépôt"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Depot_Christ_Roi));
+
+        LatLng Union_Baptiste = new LatLng(18.548707, -72.320770);
+        mMap.addMarker(new MarkerOptions().position(Union_Baptiste).title("Union Baptiste"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Union_Baptiste));
+
+        LatLng CentreSportifdeCarrefour = new LatLng(18.536234, -72.406154);
+        mMap.addMarker(new MarkerOptions().position(CentreSportifdeCarrefour).title("Centre Sportif de Carrefour"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CentreSportifdeCarrefour));
+    }
+
+    private void getAllVendors(){
 
     }
 }
