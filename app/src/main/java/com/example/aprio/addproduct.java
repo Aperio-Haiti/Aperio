6a3e670e8f182fc77574c3d3a2a5934b6d8772b9
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-21 20:57:17 -0600

    AddProduct

--- app/src/main/java/com/example/aprio/addproduct.java
+++ app/src/main/java/com/example/aprio/addproduct.java
@@ -1,17 +1,27 @@
 package com.example.aprio;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.io.File;
 
@@ -20,10 +30,10 @@ import butterknife.ButterKnife;
 
 public class addproduct extends AppCompatActivity {
 
-    @BindView(R.id.post)
-    ImageView post;
-    @BindView(R.id.imagePost)
-    ImageView imagePost;
+    @BindView(R.id.Imgpost)
+    ImageView Imgpost;
+    @BindView(R.id.imgTakeImg)
+    ImageView imgTakeImg;
     @BindView(R.id.etProductTitle)
     EditText etProductTitle;
     @BindView(R.id.etCategory)
@@ -42,6 +52,26 @@ public class addproduct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addproduct);
         ButterKnife.bind(this);
+
+        imgTakeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCamera();
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser user = ParseUser.getCurrentUser();
+                String description = etProductTitle.getText().toString().trim();
+                String category = etCategory.getText().toString().trim();
+
+                saveProduct(description,user,category,photoFile);
+            }
+        });
+
+
     }
 
     private void launchCamera() {
@@ -80,5 +110,50 @@ public class addproduct extends AppCompatActivity {
         File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
 
         return file;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                Imgpost.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void saveProduct(String description, ParseUser user,String category, File photoFile) {
+        Product product = new Product();
+
+        product.set_Description(description);
+        product.set_Category(category);
+        product.set_User(user);
+        product.set_Image_Product(new ParseFile(photoFile));
+
+//        ProgressBar pb = (ProgressBar) findViewById(R.id.pbLoading);
+//        pb.setVisibility(ProgressBar.VISIBLE);
+
+        product.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null){
+                    Log.e(TAG, "done: failed");                   
+                    e.printStackTrace();
+                    return;
+                }
+                Log.d(TAG, "done: Success");
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+//                pb.setVisibility(ProgressBar.INVISIBLE);
+//                et_description.setText("");
+//                iv_post.setImageResource(0);
+            }
+        });
     }
 }
