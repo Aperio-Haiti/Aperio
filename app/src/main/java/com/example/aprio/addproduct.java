60cea67f4bc8ea544730639131c5f41575584278
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-21 19:29:42 -0600

    AddPorduct

--- app/src/main/java/com/example/aprio/addproduct.java
+++ app/src/main/java/com/example/aprio/addproduct.java
@@ -1,14 +1,32 @@
 package com.example.aprio;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class addproduct extends AppCompatActivity {
+
+    @BindView(R.id.post)
+    ImageView post;
+    @BindView(R.id.imagePost)
+    ImageView imagePost;
+    @BindView(R.id.etProductTitle)
+    EditText etProductTitle;
+    @BindView(R.id.etCategory)
+    EditText etCategory;
+    @BindView(R.id.btnPost)
+    Button btnPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addproduct);
+        ButterKnife.bind(this);
     }
 }
