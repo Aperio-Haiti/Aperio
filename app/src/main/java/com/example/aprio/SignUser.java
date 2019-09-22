53e248fac0986df6dead0484d5147049668a5b29
sauvelino <sauvelino@outlook.com>
2019-09-22 16:58:55 -0400

    version with little modification ( about flow)

--- app/src/main/java/com/example/aprio/SignUser.java
+++ app/src/main/java/com/example/aprio/SignUser.java
@@ -2,7 +2,11 @@ package com.example.aprio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignUser extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class SignUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_user);
+        TextView SaU=findViewById(R.id.tvLoginSignup);
+        Button log=findViewById(R.id.btnsignUser);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUser.this,HomeUser.class));
+            }
+        });
+
+        SaU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUser.this,Login.class));
+            }
+        });
     }
 }
