683edb460ffe9423f6dc1c58e944c6f9d7053048
BeliMackendy <belizaire.mackendy@gmail.com>
2019-09-24 15:01:45 -0600

    Master review change name activity_myproductfragment

--- app/src/main/java/com/example/aprio/Adapters/RecyclerviewAdapter.java
+++ app/src/main/java/com/example/aprio/Adapters/RecyclerviewAdapter.java
@@ -1,6 +1,9 @@
 package com.example.aprio.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +13,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.aprio.GlideApp;
+import com.example.aprio.Models.Product;
 import com.example.aprio.R;
+import com.parse.ParseException;
 
+import java.io.File;
 import java.util.List;
 
 
 public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.ViewHOlder> {
 
     private Context context;
-    private List<String> data;
+    private List<Product> data;
 
-
-
-
-    RecyclerviewAdapter(Context context, List<String> data) {
+   public RecyclerviewAdapter(Context context, List<Product> data) {
         this.context = context;
         this.data=data;
     }
@@ -37,7 +41,9 @@ public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHOlder holder, int position) {
-String title=data.get(position);
+        Product p = data.get(position);
+        holder.bind(p);
+//        String title=data.get(position);
 
     }
 
@@ -59,5 +65,42 @@ String title=data.get(position);
             name=itemView.findViewById(R.id.tvMyproductname);
             category=itemView.findViewById(R.id.tvMyproductcategory);
         }
+
+        public void bind(Product p) {
+            name.setText(p.get_Description());
+            Log.d("Category", "bind: "+p.get_Category().getString(p.KEY_CATEGORY));
+            category.setText(p.get_Category().getString(p.KEY_CATEGORY));
+            File photoFile = null;
+            try {
+                photoFile = p.get_Image_Product().getFile();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+            GlideApp.with(context)
+                    .load(takenImage)
+                    .into(imgpost);
+        }
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+
+        data.clear();
+
+        notifyDataSetChanged();
+
+    }
+
+    // Add a list of items -- change to type used
+
+    public void addProduct(List<Product> list) {
+
+        data.addAll(list);
+
+        notifyDataSetChanged();
+
     }
 }
--- app/src/main/java/com/example/aprio/Fragment/FragmentProductsSeller.java
+++ app/src/main/java/com/example/aprio/Fragment/FragmentProductsSeller.java
@@ -2,6 +2,7 @@ package com.example.aprio.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,31 +10,48 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.aprio.Adapters.RecyclerviewAdapter;
+import com.example.aprio.Models.Product;
 import com.example.aprio.R;
 import com.example.aprio.AddProduct;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class FragmentProductsSeller extends Fragment {
     @BindView(R.id.rvproduct)
     RecyclerView rvproduct;
     @BindView(R.id.fab)
     FloatingActionButton fab;
+
+    @BindView(R.id.swipeContainer)
+    SwipeRefreshLayout swipeContainer;
+
     //ok
-//    private RecyclerviewAdapter rvAdapter;
+    private RecyclerviewAdapter rvAdapter;
 //    private RecyclerView recyclerView;
-//    private ArrayList<String> item;
+    private List<Product> item;
+    Unbinder unbinder ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return inflater.inflate(R.layout.activity_myproductfragment, container, false);
         View view = inflater.inflate(R.layout.activity_myproductfragment, container, false);
-        ButterKnife.bind(this, view);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
@@ -47,7 +65,12 @@ public class FragmentProductsSeller extends Fragment {
                 startActivity(new Intent(getContext(), AddProduct.class));
             }
         });
-//        item = new ArrayList<>();
+
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+
+//
+        item = new ArrayList<>();
 //        item.add("1");
 //        item.add("2");
 //        item.add("4");
@@ -55,10 +78,63 @@ public class FragmentProductsSeller extends Fragment {
 //        item.add("7");
 //        recyclerView = view.findViewById(R.id.rvproduct);
 //
-//        rvAdapter = new RecyclerviewAdapter(getActivity(), item);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        rvAdapter = new RecyclerviewAdapter(getContext(), item);
+        rvproduct.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
 //
-//        recyclerView.setAdapter(rvAdapter);
+        rvproduct.setAdapter(rvAdapter);
+            queryProduct();
+
+            swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                    android.R.color.holo_green_light,
+                    android.R.color.holo_orange_light,
+                    android.R.color.holo_red_light);
+
+            swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    queryProduct();
+
+                }
+            });
+        }
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder .unbind();
+    }
+
+    protected void queryProduct() {
+        ParseQuery<Product> postQuery = ParseQuery.getQuery(Product.class);
+        postQuery.include(Product.KEY_USER);
+        postQuery.include(Product.KEY_CATEGORY);
+        postQuery.whereEqualTo(Product.KEY_USER, ParseUser.getCurrentUser());
+
+        postQuery.findInBackground(new FindCallback<Product>() {
+            @Override
+            public void done(List<Product> objects, ParseException e) {
+                List<Product>  ProductToAdd = new ArrayList<>();
+                if (e == null) {
+
+                    for (int i = 0; i < objects.size(); i++) {
+                        Product p = objects.get(i);
+                        ProductToAdd.add(p);
+                    }
+//                    posts.addAll(objects);
+//                    adapter.notifyDataSetChanged();
+                } else {
+                    Log.e("Error", "Error : " + e.getMessage());
+                    e.printStackTrace();
+                    return;
+                }
+
+                rvAdapter.clear();
+                rvAdapter.addProduct(ProductToAdd);
+                swipeContainer.setRefreshing(false);
+            }
+        });
+    }
+
 }
 
